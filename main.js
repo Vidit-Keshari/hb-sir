ddb14ab5c9cf85f43df1ae80ce70db1cd0448374
Vidit Keshari <95676105+Vidit-Keshari@users.noreply.github.com>
2024-05-01 22:10:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function ABPS() {
+    window.alert("Admission form in the webpage doesn't works so you can fill up the form and check, No details are being sent.");
+    window.open("https://vidit-keshari.github.io/ABPS-RAWAN/");
+}
+
+function footer() {
+    window.alert("This webpage is created by Vidit Keshari, copyright, @All rights reserved");
+}
+
+function thanks() {
+    window.alert("Great Thanks sir");
+    document.getElementById("thankyou").innerHTML = '<section id="thanksv"><iframe width="718" height="404" src="https://www.youtube.com/embed/_bxZ6joLWCM" title="&quot;A Little Thank You&quot; Original Song of Gratitude by Sherri Boekweg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br><br><br><h4 class="btn btn-warning">Great Thankyou Sir</h4></section>';
+    document.getElementById("text-change").innerHTML = 'Thankyou Video';
+    document.getElementById("text-change-karo").innerHTML = 'Thankyou Sir';
+}
